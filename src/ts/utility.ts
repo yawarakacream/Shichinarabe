4717d0721cbf7ed9bbe0cf8bb37adf77cf040a32
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2020-12-31 03:55:21 +0900

    Remove async event handling functions

--- src/ts/utility.ts
+++ src/ts/utility.ts
@@ -9,23 +9,6 @@ export const shuffleArray = <T>(array: T[]): T[] => {
 	return result;
 };
 
-export const newArray = <T>(size: number, elementCreator: (index: number) => T): T[] => {
-	return new Array(size).fill(null).map((v, i) => elementCreator(i));
-};
-
-export const listenEventOnceAsync = async <K extends keyof HTMLElementEventMap>(target: HTMLElement, type: K): Promise<HTMLElementEventMap[K]> => {
-		return new Promise(resolve => target.addEventListener(type, ev => {
-			resolve(ev);
-			return ev;
-		}, { once: true }));
-	};
-
-export const awaitEvent = async <K extends keyof HTMLElementEventMap>(target: HTMLElement, type: K,
-	filter: (target: HTMLElement, ev: HTMLElementEventMap[K]) => boolean) => {
-		while (true) {
-			if (await listenEventOnceAsync(target, type).then(ev => filter(target, ev)))
-				break;
-		}
-	};
+export const newArray = <T>(size: number, elementCreator: (index: number) => T): T[] => new Array(size).fill(null).map((_, i) => elementCreator(i));
 
 export const awaitSleep = async (ms: number): Promise<void> => new Promise<void>(resolve => setTimeout(() => resolve(), ms));
