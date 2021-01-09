8d41b329df765cb961ebd693ce579c96963d85d6
yawarakacream <70136871+yawarakacream@users.noreply.github.com>
2021-01-09 23:27:16 +0900

    Fix quotation marks

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    mode: 'development',
-    entry: './src/ts/index.ts',
-    output: {
-        path: __dirname,
-        filename: './src/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    }
-}
+	mode: "development",
+	entry: "./src/ts/index.ts",
+	output: {
+		path: __dirname,
+		filename: "./src/bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+			},
+		],
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+};
